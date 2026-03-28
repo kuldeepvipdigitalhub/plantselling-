6213b95b3eb08d96ba851f155d289f903346fdb3
KULDEEP HARINKHEDE <kuldeepvipdigitalhub@gmail.com>
2026-03-28 11:07:52 +0530

    Delete seller_area directory

--- seller_area/seller_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
- session_start();
- session_unset();
- session_destroy();
- echo"<script>alert('Variable destroy')</script>";
-        echo"<script>window.open('../webhome.php ', '_self')</script>";
-
- ?>
